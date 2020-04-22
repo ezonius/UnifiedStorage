5ca0c26a80265348cca575b1f0e68e94fafdddac
ezonius <ezonius@pm.me>
2020-04-22 22:46:52 +0200

    moved stuff around

--- src/main/java/ezonius/unifiedstorage/block/entity/BlockEntityMergedInventory.java
+++ src/main/java/ezonius/unifiedstorage/block/entity/BlockEntityMergedInventory.java
@@ -1,17 +1,20 @@
 package ezonius.unifiedstorage.block.entity;
 
+import ezonius.unifiedstorage.UnifiedStorage;
 import ezonius.unifiedstorage.inventory.MergedInventoriesInterface;
 import net.minecraft.block.entity.*;
 import net.minecraft.container.Container;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
-import net.minecraft.text.Text;
 import net.minecraft.util.DefaultedList;
 import net.minecraft.util.Pair;
+import net.minecraft.util.math.Direction;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public abstract class BlockEntityMergedInventory extends LootableContainerBlockEntity implements MergedInventoriesInterface {
@@ -117,4 +120,84 @@ public abstract class BlockEntityMergedInventory extends LootableContainerBlockE
     protected Container createContainer(int i, PlayerInventory playerInventory) {
         return null;
     }
+
+    @Override
+    public boolean isInvEmpty() {
+        return getInventories().stream().allMatch(LootableContainerBlockEntity::isInvEmpty);
+    }
+
+    @Override
+    public ItemStack getInvStack(int slot) {
+        Pair<Integer, Integer> targetSlot = getInvSlotMap().get(slot);
+        return getInventories().get(targetSlot.getLeft()).getInvStack(targetSlot.getRight());
+    }
+
+    @Override
+    public ItemStack takeInvStack(int slot, int amount) {
+        Pair<Integer, Integer> targetSlot = getInvSlotMap().get(slot);
+        return getInventories().get(targetSlot.getLeft()).takeInvStack(targetSlot.getRight(), amount);
+    }
+
+    @Override
+    public ItemStack removeInvStack(int slot) {
+        Pair<Integer, Integer> targetSlot = getInvSlotMap().get(slot);
+        return getInventories().get(targetSlot.getLeft()).removeInvStack(targetSlot.getRight());
+    }
+
+    @Override
+    public void setInvStack(int slot, ItemStack stack) {
+        Pair<Integer, Integer> targetSlot = getInvSlotMap().get(slot);
+        getInventories().get(targetSlot.getLeft()).setInvStack(targetSlot.getRight(), stack);
+    }
+
+    @Override
+    public int getInvMaxStackAmount() {
+        return UnifiedStorage.MAX_STACK_SIZE;
+    }
+
+    @Override
+    public void markDirty() {
+        getInventories().forEach(LootableContainerBlockEntity::markDirty);
+    }
+
+    @Override
+    public void onInvOpen(PlayerEntity player) {
+        getInventories().get(0).onInvOpen(player);
+    }
+
+    @Override
+    public void onInvClose(PlayerEntity player) {
+        getInventories().get(0).onInvClose(player);
+    }
+
+    @Override
+    public boolean isValidInvStack(int slot, ItemStack stack) {
+        Pair<Integer, Integer> targetSlot = getInvSlotMap().get(slot);
+        return getInventories().get(targetSlot.getLeft()).isValidInvStack(targetSlot.getRight(), stack);
+    }
+
+    @Override
+    public void clear() {
+        getInventories().forEach(LootableContainerBlockEntity::clear);
+    }
+
+
+    @Override
+    public int[] getInvAvailableSlots(Direction side) {
+        return IntStream.range(0, getInvSize()).toArray();
+    }
+
+    @Override
+    public boolean canInsertInvStack(int slot, ItemStack stack, Direction dir) {
+        ItemStack targetStack = getInvStack(slot);
+        return stack.isEmpty() ||
+                (targetStack.getItem() == stack.getItem() &&
+                        targetStack.getCount() + stack.getCount() <= this.getInvMaxStackAmount());
+    }
+
+    @Override
+    public boolean canExtractInvStack(int slot, ItemStack stack, Direction dir) {
+        return !getInvStack(slot).isEmpty();
+    }
+
 }
--- src/main/java/ezonius/unifiedstorage/inventory/MergedInventoriesInterface.java
+++ src/main/java/ezonius/unifiedstorage/inventory/MergedInventoriesInterface.java
@@ -24,32 +24,32 @@ import java.util.stream.Stream;
 
 public interface MergedInventoriesInterface extends SidedInventory, Nameable {
 
-    public default void UpdateInventories() {
+    default void UpdateInventories() {
         setInventories(calcInventories());
         setInvSize(calcInvSize());
         setInvSlotMap(calcInvSlotMap());
     }
 
     @Override
-    public int getInvSize();
-    public void setInvSize(int invSize);
-    public default int calcInvSize() {
+    int getInvSize();
+    void setInvSize(int invSize);
+    default int calcInvSize() {
         return getInventories().stream()
                 .map(LootableContainerBlockEntity::getInvSize)
                 .reduce(Integer::sum)
                 .orElse(this.getInvSize());
     }
 
-    public ArrayList<LootableContainerBlockEntity> getInventories();
-    public void setInventories(ArrayList<LootableContainerBlockEntity> inventories);
-    public default ArrayList<LootableContainerBlockEntity> calcInventories() {
+    ArrayList<LootableContainerBlockEntity> getInventories();
+    void setInventories(ArrayList<LootableContainerBlockEntity> inventories);
+    default ArrayList<LootableContainerBlockEntity> calcInventories() {
         return getRecursiveAdjacentEntities(new HashSet<>())
                 .collect(Collectors.toCollection(ArrayList::new));
     }
 
-    public HashMap<Integer, Pair<Integer, Integer>> getInvSlotMap();
-    public void setInvSlotMap(HashMap<Integer, Pair<Integer, Integer>> invSlotMap);
-    public default HashMap<Integer, Pair<Integer, Integer>> calcInvSlotMap() {
+    HashMap<Integer, Pair<Integer, Integer>> getInvSlotMap();
+    void setInvSlotMap(HashMap<Integer, Pair<Integer, Integer>> invSlotMap);
+    default HashMap<Integer, Pair<Integer, Integer>> calcInvSlotMap() {
         HashMap<Integer, Pair<Integer, Integer>> invSlotMap = new HashMap<>();
         int slot = 0;
         for (int i = 0; i < getInventories().size(); i++) {
@@ -61,81 +61,76 @@ public interface MergedInventoriesInterface extends SidedInventory, Nameable {
         return invSlotMap;
     }
 
-    public Stream<LootableContainerBlockEntity> getRecursiveAdjacentEntities(HashSet<LootableContainerBlockEntity> checkList);
+    Stream<LootableContainerBlockEntity> getRecursiveAdjacentEntities(HashSet<LootableContainerBlockEntity> checkList);
 
     @Override
-    public default boolean isInvEmpty() {
+    default boolean isInvEmpty() {
         return getInventories().stream().allMatch(LootableContainerBlockEntity::isInvEmpty);
     }
 
     @Override
-    public default ItemStack getInvStack(int slot) {
+    default ItemStack getInvStack(int slot) {
         Pair<Integer, Integer> targetSlot = getInvSlotMap().get(slot);
         return getInventories().get(targetSlot.getLeft()).getInvStack(targetSlot.getRight());
     }
 
     @Override
-    public default ItemStack takeInvStack(int slot, int amount) {
+    default ItemStack takeInvStack(int slot, int amount) {
         Pair<Integer, Integer> targetSlot = getInvSlotMap().get(slot);
         return getInventories().get(targetSlot.getLeft()).takeInvStack(targetSlot.getRight(), amount);
     }
 
     @Override
-    public default ItemStack removeInvStack(int slot) {
+    default ItemStack removeInvStack(int slot) {
         Pair<Integer, Integer> targetSlot = getInvSlotMap().get(slot);
         return getInventories().get(targetSlot.getLeft()).removeInvStack(targetSlot.getRight());
     }
 
     @Override
-    public default void setInvStack(int slot, ItemStack stack) {
+    default void setInvStack(int slot, ItemStack stack) {
         Pair<Integer, Integer> targetSlot = getInvSlotMap().get(slot);
         getInventories().get(targetSlot.getLeft()).setInvStack(targetSlot.getRight(), stack);
     }
 
     @Override
-    public default int getInvMaxStackAmount() {
+    default int getInvMaxStackAmount() {
         return UnifiedStorage.MAX_STACK_SIZE;
     }
 
     @Override
-    public default void markDirty() {
+    default void markDirty() {
         getInventories().forEach(LootableContainerBlockEntity::markDirty);
     }
 
     @Override
-    public default boolean canPlayerUseInv(PlayerEntity player) {
-        return true;
-    }
-
-    @Override
-    public default void onInvOpen(PlayerEntity player) {
+    default void onInvOpen(PlayerEntity player) {
         getInventories().get(0).onInvOpen(player);
     }
 
     @Override
-    public default void onInvClose(PlayerEntity player) {
+    default void onInvClose(PlayerEntity player) {
         getInventories().get(0).onInvClose(player);
     }
 
     @Override
-    public default boolean isValidInvStack(int slot, ItemStack stack) {
+    default boolean isValidInvStack(int slot, ItemStack stack) {
         Pair<Integer, Integer> targetSlot = getInvSlotMap().get(slot);
         return getInventories().get(targetSlot.getLeft()).isValidInvStack(targetSlot.getRight(), stack);
     }
 
     @Override
-    public default void clear() {
+    default void clear() {
         getInventories().forEach(LootableContainerBlockEntity::clear);
     }
 
 
     @Override
-    public default int[] getInvAvailableSlots(Direction side) {
+    default int[] getInvAvailableSlots(Direction side) {
         return IntStream.range(0, getInvSize()).toArray();
     }
 
     @Override
-    public default boolean canInsertInvStack(int slot, ItemStack stack, Direction dir) {
+    default boolean canInsertInvStack(int slot, ItemStack stack, Direction dir) {
         ItemStack targetStack = getInvStack(slot);
         return stack.isEmpty() ||
                 (targetStack.getItem() == stack.getItem() &&
@@ -143,7 +138,7 @@ public interface MergedInventoriesInterface extends SidedInventory, Nameable {
     }
 
     @Override
-    public default boolean canExtractInvStack(int slot, ItemStack stack, Direction dir) {
+    default boolean canExtractInvStack(int slot, ItemStack stack, Direction dir) {
         return !getInvStack(slot).isEmpty();
     }
 }
